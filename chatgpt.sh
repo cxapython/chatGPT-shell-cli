755083ad3ce3aa350096938e10735cc81c1955f8
Chen Xiang‘An <49929318+cxapython@users.noreply.github.com>
2023-03-07 13:03:21 +0800

    Update chatgpt.sh

--- chatgpt.sh
+++ chatgpt.sh
@@ -24,7 +24,7 @@ request_to_completions() {
 	response=$(curl https://api.openai.com/v1/completions \
 		-sS \
 		-H 'Content-Type: application/json' \
-		-H "Authorization: Bearer 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" \
+		-H "Authorization: Bearer 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" \
 		-d '{
   			"model": "'"$MODEL"'",
   			"prompt": "'"${request_prompt}"'",
@@ -40,7 +40,7 @@ request_to_image() {
 	image_response=$(curl https://api.openai.com/v1/images/generations \
 		-sS \
 		-H 'Content-Type: application/json' \
-		-H "Authorization: Bearer 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" \
+		-H "Authorization: Bearer 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" \
 		-d '{
     		"prompt": "'"${prompt#*image:}"'",
     		"n": 1,
@@ -55,7 +55,7 @@ request_to_chat() {
 	response=$(curl https://api.openai.com/v1/chat/completions \
 		-sS \
 		-H 'Content-Type: application/json' \
-		-H "Authorization: Bearer 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" \
+		-H "Authorization: Bearer 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" \
 		-d '{
             "model": "'"$MODEL"'",
             "messages": [
@@ -268,14 +268,14 @@ while $running; do
 	elif [[ "$prompt" == "models" ]]; then
 		models_response=$(curl https://api.openai.com/v1/models \
 			-sS \
-			-H "Authorization: Bearer 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")
+			-H "Authorization: Bearer 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")
 		handle_error "$models_response"
 		models_data=$(echo $models_response | jq -r -C '.data[] | {id, owned_by, created}')
 		echo -e "${CHATGPT_CYAN_LABEL}This is a list of models currently available at OpenAI API:\n ${models_data}"
 	elif [[ "$prompt" =~ ^model: ]]; then
 		models_response=$(curl https://api.openai.com/v1/models \
 			-sS \
-			-H "Authorization: Bearer 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")
+			-H "Authorization: Bearer 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")
 		handle_error "$models_response"
 		model_data=$(echo $models_response | jq -r -C '.data[] | select(.id=="'"${prompt#*model:}"'")')
 		echo -e "${CHATGPT_CYAN_LABEL}Complete details for model: ${prompt#*model:}\n ${model_data}"
